14b2d7746fa704515f70f51de30c4e04a287588e
Heikki Ikäheimonen <heikki.ikaheimonen@gmail.com>
2021-02-19 22:39:37 +0200

    added !roll command, uses roll lib. usage !roll die e.g. !roll 1d6

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 require('dotenv').config()
+let Roll = require('roll')
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const roll = new Roll();
+
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+  const words = msg.content.split(' ');
+  console.log(words);
+  if (words[0] === 'ping') {
+    msg.reply('pong');
+  }
+  else if(words[0] === '!roll'){
+    if(words.length === 1){
+      msg.reply("anna nopat!");
+    }
+    else{
+      try{
+        let rollArgs = msg.content.substring(msg.content.indexOf(' ')+1);
+        let diceVal = roll.roll(rollArgs);
+        if(diceVal.rolled.length === 1){
+          msg.reply(diceVal.result);
+        }
+        else{
+          msg.reply(diceVal.result + " (" + diceVal.rolled + ")");
+        }
+      }
+      catch(e){
+        console.log(e);
+      }
+    }
+  }
+  
+});
 
 client.login(process.env.BOT_TOKEN);
